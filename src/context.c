mightyCelu/libva-v4l2-request

Add additional compressed frame formats
These are supported by the (stateful) meson-vdec driver. The matching of
libVA profiles to V4L2 pixel formats is not the cleanest, but a rework
can wait until this actually works.

src/context.c
@@ -99,7 +99,14 @@ VAStatus RequestCreateContext(VADriverContextP context, VAConfigID config_id,
 	case VAProfileH264ConstrainedBaseline:
 	case VAProfileH264MultiviewHigh:
 	case VAProfileH264StereoHigh:
-		pixelformat = V4L2_PIX_FMT_H264_SLICE;
+		if (v4l2_find_format(driver_data->video_fd, capture_type, V4L2_PIX_FMT_H264)) {
+			pixelformat = V4L2_PIX_FMT_H264;
+		} else if (v4l2_find_format(driver_data->video_fd, capture_type, V4L2_PIX_FMT_H264_SLICE)) {
+			pixelformat = V4L2_PIX_FMT_H264_SLICE;
+		} else {
+			status = VA_STATUS_ERROR_UNSUPPORTED_PROFILE;
+			goto error;
+		}
 		break;
 
 	case VAProfileHEVCMain:
@@ -114,7 +121,14 @@ VAStatus RequestCreateContext(VADriverContextP context, VAConfigID config_id,
 	case VAProfileVP9Profile1:
 	case VAProfileVP9Profile2:
 	case VAProfileVP9Profile3:
-		pixelformat = V4L2_PIX_FMT_VP9_FRAME;
+		if (v4l2_find_format(driver_data->video_fd, capture_type, V4L2_PIX_FMT_VP9)) {
+			pixelformat = V4L2_PIX_FMT_VP9;
+		} else if (v4l2_find_format(driver_data->video_fd, capture_type, V4L2_PIX_FMT_VP9_FRAME)) {
+			pixelformat = V4L2_PIX_FMT_VP9_FRAME;
+		} else {
+			status = VA_STATUS_ERROR_UNSUPPORTED_PROFILE;
+			goto error;
+		}
 		break;
 
 	default:

phxinyang/qualcomm-iris-vaapi

src/context.c
@@ -99,7 +99,14 @@ VAStatus RequestCreateContext(VADriverContextP context, VAConfigID config_id,
 	case VAProfileH264ConstrainedBaseline:
 	case VAProfileH264MultiviewHigh:
 	case VAProfileH264StereoHigh:
-		pixelformat = V4L2_PIX_FMT_H264_SLICE;
+		if (v4l2_find_format(driver_data->video_fd, output_type, V4L2_PIX_FMT_H264)) {
+			pixelformat = V4L2_PIX_FMT_H264;
+		} else if (v4l2_find_format(driver_data->video_fd, output_type, V4L2_PIX_FMT_H264_SLICE)) {
+			pixelformat = V4L2_PIX_FMT_H264_SLICE;
+		} else {
+			status = VA_STATUS_ERROR_UNSUPPORTED_PROFILE;
+			goto error;
+		}
 		break;
 
 	case VAProfileHEVCMain:
@@ -114,7 +121,14 @@ VAStatus RequestCreateContext(VADriverContextP context, VAConfigID config_id,
 	case VAProfileVP9Profile1:
 	case VAProfileVP9Profile2:
 	case VAProfileVP9Profile3:
-		pixelformat = V4L2_PIX_FMT_VP9_FRAME;
+		if (v4l2_find_format(driver_data->video_fd, output_type, V4L2_PIX_FMT_VP9)) {
+			pixelformat = V4L2_PIX_FMT_VP9;
+		} else if (v4l2_find_format(driver_data->video_fd, output_type, V4L2_PIX_FMT_VP9_FRAME)) {
+			pixelformat = V4L2_PIX_FMT_VP9_FRAME;
+		} else {
+			status = VA_STATUS_ERROR_UNSUPPORTED_PROFILE;
+			goto error;
+		}
 		break;
 
 	default: